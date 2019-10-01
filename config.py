1fb415b04d0e5daa4f1a71c5e38915df61c17a6f
chi <w121210@hotmail.com>
2019-10-01 20:39:07 +0700

    .

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 seed = 0
 
-dataset_name = 'ICDAR2013_ICDAR2017'
-test_dataset_name = 'ICDAR2013'
+dataset_name = "ICDAR2013_ICDAR2017"
+test_dataset_name = "ICDAR2013"
 
-DataLoaderSYNTH_base_path = '/home/SharedData/Mayank/SynthText/Images'
-DataLoaderSYNTH_mat = '/home/SharedData/Mayank/SynthText/gt.mat'
-DataLoaderSYNTH_Train_Synthesis = '/home/SharedData/Mayank/Models/SYNTH/train_synthesis/'
+DataLoaderSYNTH_base_path = "/notebooks/CRAFT-Remade/SynthText/Images"
+DataLoaderSYNTH_mat = "/notebooks/CRAFT-Remade/SynthText/gt.mat"
+DataLoaderSYNTH_Train_Synthesis = "/notebooks/CRAFT-Remade/model/SYNTH/train_synthesis/"
 
-DataLoader_Other_Synthesis = '/home/SharedData/Mayank/'+dataset_name+'/Save/'
-Other_Dataset_Path = '/home/SharedData/Mayank/'+dataset_name
-save_path = '/home/SharedData/Mayank/Models/WeakSupervision/'+dataset_name
-images_path = '/home/SharedData/Mayank/'+dataset_name+'/Images'
-target_path = '/home/SharedData/Mayank/'+dataset_name+'/Generated'
+DataLoader_Other_Synthesis = "/notebooks/CRAFT-Remade" + dataset_name + "/Save/"
+Other_Dataset_Path = "/notebooks/CRAFT-Remade" + dataset_name
+save_path = "/notebooks/CRAFT-Remade/Models/WeakSupervision/" + dataset_name
+images_path = "/notebooks/CRAFT-Remade/" + dataset_name + "/Images"
+target_path = "/notebooks/CRAFT-Remade/" + dataset_name + "/Generated"
 
-Test_Dataset_Path = '/home/SharedData/Mayank/'+test_dataset_name
+Test_Dataset_Path = "/notebooks/CRAFT-Remade/" + test_dataset_name
 
 threshold_character = 0.5
 threshold_affinity = 0.5
@@ -21,24 +21,12 @@ threshold_word = 0.7
 threshold_fscore = 0.5
 
 dataset_pre_process = {
-	'ic13': {
-		'train': {
-			'target_json_path': None,
-			'target_folder_path': None,
-		},
-		'test': {
-			'target_json_path': None,
-			'target_folder_path': None,
-		}
-	},
-	'ic15': {
-		'train': {
-			'target_json_path': None,
-			'target_folder_path': None,
-		},
-		'test': {
-			'target_json_path': None,
-			'target_folder_path': None,
-		}
-	}
+    "ic13": {
+        "train": {"target_json_path": None, "target_folder_path": None},
+        "test": {"target_json_path": None, "target_folder_path": None},
+    },
+    "ic15": {
+        "train": {"target_json_path": None, "target_folder_path": None},
+        "test": {"target_json_path": None, "target_folder_path": None},
+    },
 }
--- train_synth/config.py
+++ train_synth/config.py
@@ -1,33 +1,32 @@
 """
 Config containing all hardcoded parameters for training strong supervised model on synth-text
 """
+import os
 from config import *
 
 num_cuda = "0,1,2,3"
-save_path = '/home/SharedData/Mayank/Models/SYNTH'
-use_cuda = True
+root = "/notebooks/CRAFT-Remade/"
+
+# save_path = "/home/SharedData/Mayank/Models/SYNTH"
+save_path = os.path.join(root, "model")
+# use_cuda = True
+use_cuda = False
 
 batch_size = {
-	'train': 4*len(num_cuda.split(',')),
-	'test': 8*len(num_cuda.split(',')),
+    "train": 4 * len(num_cuda.split(",")),
+    "test": 8 * len(num_cuda.split(",")),
 }
 
-num_workers = {
-	'train': 16,
-	'test': 16
-}
+num_workers = {"train": 16, "test": 16}
 
-pretrained = True
-pretrained_path = '/home/SharedData/Mayank/Models/SYNTH/63000_model.pkl'
-pretrained_loss_plot_training = '/home/SharedData/Mayank/Models/SYNTH/loss_plot_training.npy'
+# pretrained = True
+pretrained = False
+pretrained_path = "/home/SharedData/Mayank/Models/SYNTH/63000_model.pkl"
+pretrained_loss_plot_training = (
+    "/home/SharedData/Mayank/Models/SYNTH/loss_plot_training.npy"
+)
 
-lr = {
-	1: 1e-4,
-	10000: 5e-5,
-	20000: 2e-5,
-	40000: 1e-5,
-	60000: 1e-6,
-}
+lr = {1: 1e-4, 10000: 5e-5, 20000: 2e-5, 40000: 1e-5, 60000: 1e-6}
 
 num_epochs_strong_supervision = 2
 
@@ -40,6 +39,6 @@ visualize_generated = False
 
 weight_threshold = 0.5
 
-model_architecture = 'craft'
+model_architecture = "craft"
 
 image_size = [1024, 1024]
